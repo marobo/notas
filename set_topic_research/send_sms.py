d7d4118978afb768a4654bb4aed11ccbbdfee325
Onorio de Jesus Afonso <onorio@catalpa.io>
2020-10-12 15:20:18 +0900

    remove secret api

--- set_topic_research/send_sms.py
+++ set_topic_research/send_sms.py
@@ -3,11 +3,11 @@
 from twilio.rest import Client
 
 # Find these values at https://twilio.com/user/account
-account_sid = "ACa2917b3dd13fb0699457988a1a45453b"
-auth_token = "94f8bb4ea3334a95fc06d094b01bc6a2"
+account_sid = "your_account_sid_here"
+auth_token = "your_auth_token_here"
 client = Client(account_sid, auth_token)
 
-message = client.api.account.messages.create(to="+67078150465",
-                                             from_="+14253201886",
+message = client.api.account.messages.create(to="+670xxxxxxxx",
+                                             from_="+142xxxxxxxxx",
                                              body="MALUK SIRA IMI DIAK KA LAE?")
 
